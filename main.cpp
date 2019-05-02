c1e21fae1d0ce332ac1f61ce6ff4089a1c095db9
youwereeatenbyalid <youwereeatenbyalid@github.com>
2019-05-01 22:26:00 -0400

    Wrote some algorithms.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
 
+#include<list>
+#include"algorithms.cpp"
+/*
+template<typename data>
+
+void selectionsort(std::list<data> &myList){
+    typename std::list<data>::iterator iterator_front = myList.begin();
+
+    while(iterator_front!=myList.end()){
+        typename std::list<data>::iterator iterator_traverse = iterator_front;
+        typename std::list<data>::iterator iterator_smallest = iterator_front;
+        iterator_traverse++;
+        while(iterator_traverse!=myList.end()){
+            if(*iterator_traverse<*iterator_smallest) iterator_smallest = iterator_traverse;
+            
+            iterator_traverse++;
+        }
+    data temp = *iterator_front;
+    *iterator_front = *iterator_smallest;
+    *iterator_smallest = temp;
+    iterator_front++;
+    }
+}
+*/
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+    std::list<int> myList = {5,1,2,9,3,19,23,4};
+    int myArray[9] = {0,5,1,2,9,3,19,23,4};
+
+    selectionsort(myList);
+
+
+    std::cout << std::endl;
+    bubblesort(myArray,9);
+
+
+return 0;
 }
